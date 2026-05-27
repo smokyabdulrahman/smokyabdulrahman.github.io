4059500d6d768b690024e09ecdbb25fda260af2c
Abdulrahman Alrahma <a.alrahama@gmail.com>
2026-05-27 19:47:14 +0100

    first

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "abdulrahman alrahma",
+	url: "https://alrahma.me/",
+	language: "en",
+	description: "Personal site and blog of abdulrahman alrahma.",
+	author: {
+		name: "abdulrahman alrahma",
+		email: "abdulrahman@alrahma.me",
+		url: "https://alrahma.me/about/"
+	}
+}
